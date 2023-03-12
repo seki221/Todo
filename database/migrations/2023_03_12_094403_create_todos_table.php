30fb91c0967930042cd7922840822901991f458f
seki221 <pawls00.bugaboo@icloud.com>
2023-03-12 12:25:47 +0900

    mig c

--- database/migrations/2023_03_12_094403_create_todos_table.php
+++ database/migrations/2023_03_12_094403_create_todos_table.php
@@ -15,7 +15,9 @@ class CreateTodosTable extends Migration
     {
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('content', 20);
+            $table->timestamp('created_at')->useCurrent()->nullable();
+            $table->timestamp('updated_at')->useCurrent()->nullable();
         });
     }
 
