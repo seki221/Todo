6b6b67c899b013156ede848995afdc2ea3b913a4
seki221 <pawls00.bugaboo@icloud.com>
2023-03-13 02:57:27 +0900

    delete c

---
+++ resources/views/delete.blade.php
@@ -0,0 +1,28 @@
+      @foreach ($todos as $todo)
+      <tr>
+        <td>{{$todo->created_at}}</id>
+        <td>
+          <input type="text" name="task" class="input-update" value={{$todo->content}}>
+        </td>
+        <td class="eb">
+          <div>
+            <a href="/edit{{ content->$content }}" class="edit">
+              <button class="button_edit">更新</button>
+            </a>
+          </div>
+        </td>
+        <td>
+          <div class="db">
+            <form action="/dalete/{{ $todo->id }}" method="post" class="delete" role="menuitem" tabindex="-1">
+              @csrf
+              @method('DELETE')
+              <button type="submit" class="button_delete">削除</button>
+              <input type="text" name="task" class="input-update" value={{$todo->content}}>
+            </form>
+          </div>
+        </td>
+      </tr>
+      @endforeach
+      </tbody>
+      </table>
+      </div>
