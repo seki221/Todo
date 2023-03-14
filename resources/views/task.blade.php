650ffe9378f71337d0ef16cd592c0360a00210c0
seki221 <pawls00.bugaboo@icloud.com>
2023-03-14 12:17:10 +0900

    add task

--- resources/views/task.blade.php
+++ resources/views/task.blade.php
@@ -13,7 +13,7 @@
         </td>
         <td>
           <div class="db">
-            <form action="/dalete/{{ $todo->id }}" method="post" class="delete" role="menuitem" tabindex="-1">
+            <form action="/dalete/{{ $todo->id }}" method="post" class="remove" role="menuitem" tabindex="-1">
               @csrf
               @method('DELETE')
               <button type="submit" class="button_delete">削除</button>
